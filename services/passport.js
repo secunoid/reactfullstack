4b976ab1e0b405ead903da2996eb2101087453b7
secunoid <secunoid@gmail.com>
2018-04-28 21:07:34 -0400

    updates after following code in video

--- services/passport.js
+++ services/passport.js
@@ -11,33 +11,28 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((id, done) => {
   User.findById(id)
-  .then(user => {
-    done(null, user);
-  });
+    .then(user => {
+      done(null, user);
+    });
 });
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-      },
-      (accessToken, refreshToken, profile, done) => {
-        //console.log('access token', accessToken);
-        //console.log('refresh token', refreshToken);
-        //console.log('profile', profile);
-        User.findOne({ googleId: profile.id }).then (existingUser => {
-            if (existingUser) {
-              done(null, existingUser);
-            } else {
-              new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-            }
-          });
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
       }
-    )
-  );
-  
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
